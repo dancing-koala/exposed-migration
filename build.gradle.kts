e764fa07fa503905d1ca96cb1e8645901f95da31
dancing-koala <ccbbruneau@gmail.com>
2021-05-01 18:47:57 +0200

    Add maven plugin & update version to 0.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.32"
+    maven
 }
 
 group = "fr.dancing_koala"
-version = "0.0.1"
+version = "0.0.2"
 
 repositories {
     mavenCentral()
@@ -22,6 +23,10 @@ dependencies {
 //    implementation("ch.qos.logback:logback-classic:1.2.3")
 
     testImplementation(kotlin("test-junit"))
+}
+
+tasks.test {
+    useJUnit()
 }
 
 tasks.withType<KotlinCompile>() {
